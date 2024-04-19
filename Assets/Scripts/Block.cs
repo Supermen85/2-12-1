6938b0abe13d428c6268c1087969d7c092a0d2b6
Supermen85 <Frost_1985@mail.ru>
2024-04-19 17:09:16 +0700

    Initial commit

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody))]
+[RequireComponent (typeof(MeshRenderer))]
+
+public class Block : MonoBehaviour
+{
+    [SerializeField] private Material[] _materials;
+    [SerializeField] private float _chanceToCreatePercent = 100;
+
+    public float ChanceToCreatePercent => _chanceToCreatePercent;
+
+    public void ReduceCreateChance(float chance)
+    {
+        int coefficient = 2;
+
+        _chanceToCreatePercent = chance / coefficient;
+    }
+
+    public void Push(Vector3 center)
+    {
+        float force = 10f;
+
+        Vector3 direction = (transform.position - center).normalized;
+
+        GetComponent<Rigidbody>().velocity = direction * force;
+    }
+
+    public void ReduceScale(Vector3 scale)
+    {
+        int coefficient = 2;
+
+        transform.localScale = scale / coefficient;
+    }
+
+    public void SetMaterial()
+    {
+        GetComponent<MeshRenderer>().material = _materials[Random.Range(0, _materials.Length)];
+    }
+}
---
+++ Assets/Scripts/Creation.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+public class Creation : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    [SerializeField] private Block _prefab;
+
+    private RaycastHit _hit;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) == false)
+            return;
+
+        Ray raycast = _camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(raycast, out _hit) == false)
+            return;
+
+        if (_hit.transform.TryGetComponent<Block>(out Block block) == false)
+            return;
+
+        Destroy(block.gameObject);
+
+        if (CanCreate(block))
+            CreateBlocks(block);
+    }
+
+    private void CreateBlocks(Block block)
+    {
+        int count = GetCreatingBlocksCount();
+
+        for (int i = 0; i < count; i++)
+        {
+            Block newBlock = Instantiate(_prefab, GetCreatePosition(block), Quaternion.identity);
+
+            newBlock.SetMaterial();
+            newBlock.ReduceScale(block.transform.localScale);
+            newBlock.ReduceCreateChance(block.ChanceToCreatePercent);
+            newBlock.Push(block.transform.position);
+        }
+    }
+
+    private bool CanCreate(Block block)
+    {
+        int maxChance = 100;
+
+        if (Random.Range(0, maxChance + 1) <= block.ChanceToCreatePercent)
+            return true;
+
+        return false;
+    }
+
+    private int GetCreatingBlocksCount()
+    {
+        int min = 2;
+        int max = 6;
+
+        return Random.Range(min, max + 1);
+    }
+
+    private Vector3 GetCreatePosition(Block block)
+    {
+        float deltaX = Random.Range(-10, 11) / 10f;
+        float deltaZ = Random.Range(-10, 11) / 10f;
+
+        Vector3 position = block.transform.position;
+
+        position.x += deltaX;
+        position.z += deltaZ;
+
+        return position;
+    }
+}
